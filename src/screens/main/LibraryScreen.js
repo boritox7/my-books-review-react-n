2a1cd9de9be43aa3b24caf0cac59395b3c3f9a0d
boritox <borisvaldacbba@gmail.com>
2025-02-18 14:15:24 -0400

    agregar reseña

--- src/screens/main/LibraryScreen.js
+++ src/screens/main/LibraryScreen.js
@@ -1,13 +1,13 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, FlatList, Alert } from 'react-native';
-import { Image, Text, ListItem, Button, Dialog } from '@rneui/themed';
-import { getDoc, doc, setDoc} from 'firebase/firestore';
-import {auth,db} from "../../config/firebase";
+import { Image, Text, ListItem, Button, Dialog, Input, AirbnbRating } from '@rneui/themed';
+import { getDoc, doc, setDoc, onSnapshot } from 'firebase/firestore';
+import { auth, db } from "../../config/firebase";
 import { createComponentForStaticNavigation } from '@react-navigation/native';
 
-const Item = ({item, onAgregarFavorito, onSelectBook}) => (
+const Item = ({ item, onAgregarFavorito, onSelectBook }) => (
   <ListItem bottomDivider
-    onPress={()=>onSelectBook(item)}
+    onPress={() => onSelectBook(item)}
   >
     <Image
       source={{ uri: item.imageLinks.thumbnail }}
@@ -18,7 +18,7 @@ const Item = ({item, onAgregarFavorito, onSelectBook}) => (
       <ListItem.Subtitle>{item.subtitle}</ListItem.Subtitle>
       <Button
         title="Mi favorito"
-        onPress={()=> onAgregarFavorito(item)}
+        onPress={() => onAgregarFavorito(item)}
       />
     </ListItem.Content>
   </ListItem>
@@ -28,12 +28,15 @@ export default function LibraryScreen() {
   const [books, setBooks] = useState([]);
   const [selectedBook, setSelectedBook] = useState(null);
   const [visible1, setVisible1] = useState(false);
+  const [comentario, setComentario] = useState('');
+  const [puntuacion, setPuntuacion] = useState(0);
+  const [resenas, setResenas] = useState([]);
 
   const toggleDialog1 = () => {
     setVisible1(!visible1);
   };
 
-  async function agregarFavorito(book){
+  async function agregarFavorito(book) {
     try {
       const userRef = doc(db, 'usuarios', auth.currentUser.uid);
       const docSnap = await getDoc(userRef);
@@ -41,51 +44,87 @@ export default function LibraryScreen() {
         const userProfile = docSnap.data();
         const favoritos = userProfile.favoritos ?? []
 
-        if(!favoritos.some((f)=>f.id === book.id)){
-          favoritos.push(book); 
-          await setDoc(doc(db, 'usuarios', auth.currentUser.uid),{...userProfile, favoritos});
+        if (!favoritos.some((f) => f.id === book.id)) {
+          favoritos.push(book);
+          await setDoc(doc(db, 'usuarios', auth.currentUser.uid), { ...userProfile, favoritos });
           Alert.alert("El libro fue agregado.")
         }
       }
     } catch (error) {
       console.error(error);
     }
-    
+
   }
-  function seleccionarLibro(book){
+  function seleccionarLibro(book) {
     setSelectedBook(book);
+
+    const resena = resenas.find((r) => r.id === book.id);
+    setComentario(resena?.comentario ?? '');
+    setPuntuacion(resena?.puntuacion ?? 0);
+
     toggleDialog1();
   }
-  useEffect(()=>{
-    cargarLibros()
-  },[])
+  useEffect(() => {
+    cargarLibros();
+    const subscriber = onSnapshot(doc(db, 'usuarios', auth.currentUser.uid), (docSnap) => {
+      if (docSnap.exists())
+        setResenas(docSnap.data()?.resenas ?? []);
+    });
+  
+    return subscriber;
+  }, [])
 
-  async function cargarLibros(){
+  async function cargarLibros() {
     try {
       const response = await fetch('https://reactnd-books-api.udacity.com/books', {
-        headers: {'Authorization': 'texto'}
+        headers: { 'Authorization': 'texto' }
       });
 
       const jsonResponse = await response.json();
       setBooks(jsonResponse.books);
     } catch (error) {
       console.error(error);
+    }
   }
 
+  async function guardarResena() {
+    try {
+      const userRef = doc(db, 'usuarios', auth.currentUser.uid);
+      const docSnap = await getDoc(userRef);
+      if (docSnap.exists()) {
+        const userProfile = docSnap.data();
+
+        if (!resenas.some((f) => f.id === selectedBook.id)) {
+          await setDoc(doc(db, 'usuarios', auth.currentUser.uid), { 
+            ...userProfile, 
+            resenas: [...resenas, { id: selectedBook.id, puntuacion, comentario }] 
+          });
+          Alert.alert("La reseña fue agregada.")
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      toggleDialog1();
+    }
   }
+
   return (
     <View style={styles.container}>
       <Dialog
-       isVisible={visible1}
-       onBackdropPress={toggleDialog1} 
-       >
-      <Dialog.Title title={selectedBook?.title}/>
-      <Text>Publicado: {selectedBook?.publishedDate}</Text>
-     </Dialog>
-      <FlatList 
+        isVisible={visible1}
+        onBackdropPress={toggleDialog1}
+      >
+        <Dialog.Title title={selectedBook?.title} />
+        <Text>Publicado: {selectedBook?.publishedDate}</Text>
+        <Input placeholder='Que opinas del libro?' value={comentario} onChangeText={setComentario} />
+        <AirbnbRating defaultRating={puntuacion} onFinishRating={(value) => setPuntuacion(value)} />
+        <Button title="Guardar" onPress={guardarResena} />
+      </Dialog>
+      <FlatList
         data={books}
-        renderItem={({item}) => <Item key={item.id} item={item} onAgregarFavorito={agregarFavorito} onSelectBook={seleccionarLibro}/>}
-        keyExtractor={item => item.id}/>
+        renderItem={({ item }) => <Item key={item.id} item={item} onAgregarFavorito={agregarFavorito} onSelectBook={seleccionarLibro} />}
+        keyExtractor={item => item.id} />
     </View>
   );
 }
